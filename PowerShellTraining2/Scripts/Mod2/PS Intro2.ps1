d24612deb9d74e1997c60af8ea074036ebf80a40
BZanten <Ben@van.Zanten.name>
2023-08-30 00:07:44 +0200

    Updated command to fully qualified

--- PowerShellTraining2/Scripts/Mod2/PS Intro2.ps1
+++ PowerShellTraining2/Scripts/Mod2/PS Intro2.ps1
@@ -66,28 +66,28 @@ help ForEach-Object
 # Pipeline wordt vaak gebruikt voor interactieve sessies
 #
 # Proces met meer dan 500 handles
-Get-Process | Where { $_.Handles -gt 500 } | Sort Threads | Format-Table -Property Handles,name,workingset,threads
+Get-Process | Where-Object { $_.Handles -gt 500 } | Sort-Object Threads | Format-Table -Property Handles,name,workingset,threads
 # 10 processen met de meeste geheugengebruik
-Get-Process | Sort WorkingSet | Select-Object -Last 10 | Format-Table
+Get-Process | Sort-Object WorkingSet | Select-Object -Last 10 | Format-Table
 
 # Select object -first of -last laat het originele object intact
-Get-Process | Sort WorkingSet | Select-Object -Last 10 | Get-Member
+Get-Process | Sort-Object WorkingSet | Select-Object -Last 10 | Get-Member
 # Select object alleen bepaalde properties genereert een nieuw type object
 Get-Process | Select-Object -Property Name,WorkingSet,ID
 Get-Process | Select-Object -Property Name,WorkingSet,ID | Get-Member
 
 # Grouping objects
-Get-Process | group Company -NoElement | sort Count,Name -Descending
+Get-Process | Group-Object Company -NoElement | Sort-Object Count,Name -Descending
 # Sorteren : Desc op count,   Asc op Name
-Get-Process | group Company -NoElement | sort Count,@{Expr='Name';Desc=$false} -Descending
+Get-Process | Group-Object Company -NoElement | Sort-Object Count,@{Expr='Name';Desc=$false} -Descending
 
 # Measure objects
-Dir C:\Windows -File | Measure Length -Sum -Average
-Dir C:\Windows -Recurse -File -Ea SilentlyContinue | Measure Length -Sum -Average -min -max
+Dir C:\Windows -File | Measure-Object Length -Sum -Average
+Dir C:\Windows -Recurse -File -Ea SilentlyContinue | Measure-Object Length -Sum -Average -min -max
 
 
 # Pipeline performance.  Filter zo vroeg mogelijk in de pipeline
-$S1 = { Get-WmiObject -Class Win32_Group | where Domain -eq $env:COMPUTERNAME }
+$S1 = { Get-WmiObject -Class Win32_Group | Where-Object Domain -eq $env:COMPUTERNAME }
 $S2 = { Get-WmiObject -Query "Select * from Win32_Group where domain='$Env:ComputerName'" }
 Measure-Command -Expression $S1
 Measure-Command -Expression $S2
@@ -100,8 +100,8 @@ $a = 1,2,3
 $a[0]
 $a[-1]
 
-(Get-Hotfix | Sort InstalledOn)[-1]
-Get-Hotfix | Sort InstalledOn | Select-Object -Last 1
+(Get-Hotfix | Sort-Object InstalledOn)[-1]
+Get-Hotfix | Sort-Object InstalledOn | Select-Object -Last 1
 
 
 #  -f  Format operator
@@ -111,4 +111,12 @@ Get-Hotfix | Sort InstalledOn | Select-Object -Last 1
 "User{0:D4}" -f 1
 2..20 | % { "User{0:D4}"-f $_ }
 Get-Date
-Get-Date -Format "dd-MM-yyyy"Get-Date -Format "dd-MM-yyyy HH:mm:ss"# :: Static member operators[datetime]::Now[datetime]::Now | Get-Member[math]::pi[string]::IsNullOrEmpty($a)
+Get-Date -Format "dd-MM-yyyy"
+Get-Date -Format "dd-MM-yyyy HH:mm:ss"
+
+# :: Static member operators
+[datetime]::Now
+[datetime]::Now | Get-Member
+[math]::pi
+[string]::IsNullOrEmpty($a)
+
